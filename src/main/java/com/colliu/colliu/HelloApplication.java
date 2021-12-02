dfbc65185cbee840a6a87225b4ca98f66704bb0f
Kristofer Koskunen <koskunen@student.chalmers.se>
2021-12-02 09:55:44 +0100

    Changes by ebbaw

--- src/main/java/com/colliu/colliu/HelloApplication.java
+++ src/main/java/com/colliu/colliu/HelloApplication.java
@@ -25,6 +25,33 @@ public class HelloApplication extends Application {
   }
 
   public static void main(String[] args) {
-    launch();
+    //launch();
+    new HelloApplication();
   }
+
+  HelloApplication() {
+    user = new UserMethods();
+    String email;
+    String password;
+    String firstName;
+    String lastName;
+    int graduationYear;
+    ArrayList<String> courses = new ArrayList<>();
+    ArrayList<String> interests = new ArrayList<>();
+    String program;
+
+    email = UserInput.readLine("Enter email: ");
+    password = UserInput.readLine("Enter password: ");
+    firstName = UserInput.readLine("Enter first name: ");
+    lastName = UserInput.readLine("Enter last name: ");
+    graduationYear = UserInput.readInt("Enter graduation year: ");
+    courses.add("DIT043");
+    interests.add("Hockey");
+    program = UserInput.readLine("Enter program: ");
+    try {
+      System.out.println(user.createStudent(email, password, firstName, lastName, graduationYear, program));
+    } catch (Exception exception) {
+      System.out.println("Something went wrong");
+}
+}
 }
