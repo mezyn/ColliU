4d4d4b5efd51ff0e24e12da9429f1abfa0527dd5
Kristofer Koskunen <koskunen@student.chalmers.se>
2021-12-02 20:28:57 +0100

    Finished user class - Contributers   - Kristofer   - Erik   - Mijin   - Nia   - Ebba   - William

--- src/main/java/com/colliu/colliu/HelloApplication.java
+++ src/main/java/com/colliu/colliu/HelloApplication.java
@@ -1,12 +1,20 @@
 package com.colliu.colliu;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import miscellaneous.UserInput;
+import user.UserMethods;
+
 
 public class HelloApplication extends Application {
+  private UserMethods user;
+
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,6 +25,34 @@ public class HelloApplication extends Application {
   }
 
   public static void main(String[] args) {
-    launch();
+    //launch();
+    new HelloApplication();
+  }
+
+  HelloApplication() {
+    user = new UserMethods();
+    String email;
+    String password;
+    String firstName;
+    String lastName;
+    int graduationYear;
+    ArrayList<String> courses = new ArrayList<>();
+    ArrayList<String> interests = new ArrayList<>();
+    String program;
+
+    email = "helloA@dam.gu.se";
+    password ="PassWordMyAss45!";
+    firstName = "adam";
+    lastName = "Bengtsson";
+    graduationYear = 2024;
+    courses.add("DIT043");
+    interests.add("Hockey");
+    //program = UserInput.readLine("Enter program: ");
+    program = "SEM";
+    try {
+      System.out.println(user.createStudent(email, password, firstName, lastName, graduationYear, program));
+    } catch (Exception exception) {
+      System.out.println(exception);
+    }
   }
 }
---
+++ src/main/java/user/Student.java
@@ -0,0 +1,58 @@
+package user;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+
+/** Student class.
+ *
+ */
+public class Student extends User {
+
+  private int graduationYear;
+  //private ArrayList<String> courses;
+  //private ArrayList<String> interests;
+  private String program;
+  //private boolean isAdministrator;
+
+
+  /** Constructor for Student class.
+     *
+     */
+
+  //public Student(){} //default constructor
+
+  public Student(String email, String password, String firstName, String lastName, int graduationYear, String program) throws Exception {
+    super(email, password, firstName, lastName);
+    this.graduationYear = graduationYear;
+    this.program = program;
+
+  }
+
+
+  public String getProgram(String email) {
+    return program;
+  }
+
+  public int getGraduationYear(String email) {
+    return this.graduationYear;
+  }
+
+  public void setGraduationYear(int graduationYear) {
+    this.graduationYear =  graduationYear;
+  }
+
+  public void setProgram(String program) {
+    program = this.program;
+  }
+
+  //public ArrayList<String> getCourses() {
+  //  return courses;
+  //}
+
+  //public ArrayList<String> getInterests() {
+  //  return this.interests;
+  //}
+
+}
+
