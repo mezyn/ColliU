25fbceb87a7ded45a75fc79f9305a1afa5466dee
Mijin Kim <mijin@student.chalmers.se>
2021-12-06 18:46:14 +0100

    Nia, Mijin, William

--- src/main/java/com/colliu/colliu/HelloController.java
+++ src/main/java/com/colliu/colliu/HelloController.java
@@ -1,6 +1,7 @@
 package com.colliu.colliu;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -11,4 +12,48 @@ public class HelloController {
   protected void onHelloButtonClick() {
     welcomeText.setText("Welcome to JavaFX Application!");
   }
-}
+
+  /*
+  protected String onGUEmailClick() {
+      String guEmail = UserInput.readInput();
+      return guEmail;
+    }
+  protected void onPasswordClick() {
+    String password = UserInput.readInput();
+
+  }
+  protected void onLogInClick() {
+
+    if (UserMethods.checkExistingEmail() && UserMethods.validatePassword()) {
+
+
+    }
+  }*/
+
+  @FXML
+  void onLogInClick(ActionEvent event) {
+//anything we code here for login should work
+  }
+
+
+  @FXML
+  void onForgotPassword(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onGUEmail(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onPassword(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onRegisterAccount(ActionEvent event) {
+
+  }
+  }
+
